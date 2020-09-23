2d6e6ef4ef5a4d9358534653978e8c3b05e2eea0
Rodrigo Santos <ro.augusto@gmail.com>
2020-09-23 12:27:51 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+	cards := newDeck()
+
+	hand, remainingCards := deal(cards, 5)
+
+	hand.print()
+	remainingCards.print()
+}
+
+func newCard() string {
+	return "Five of Diamonds"
+}
