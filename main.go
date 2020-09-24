e00f1905be8a763891de619a385007c6cf10b68c
Rodrigo Santos <ro.augusto@gmail.com>
2020-09-24 20:34:50 -0300

    First project with GoLang

--- main.go
+++ main.go
@@ -2,13 +2,6 @@ package main
 
 func main() {
 	cards := newDeck()
-
-	hand, remainingCards := deal(cards, 5)
-
-	hand.print()
-	remainingCards.print()
-}
-
-func newCard() string {
-	return "Five of Diamonds"
+	cards.shuffle()
+	cards.print()
 }
